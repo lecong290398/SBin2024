fc45ed98ee567e24cb1b32f23b97f7d7db209aa6
lemanh09 <ledinhducmanh09@gmail.com>
2024-09-15 22:57:08 +0700

    Fix css html add Modal

--- aspnet-core/src/DTKH2024.SbinSolution.Web.Mvc/Areas/App/Controllers/RedeemGiftsController.cs
+++ aspnet-core/src/DTKH2024.SbinSolution.Web.Mvc/Areas/App/Controllers/RedeemGiftsController.cs
@@ -6,6 +6,7 @@ using DTKH2024.SbinSolution.RedeemGifts;
 using DTKH2024.SbinSolution.Web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DTKH2024.SbinSolution.Web.Areas.App.Controllers
 {
@@ -13,20 +14,24 @@ namespace DTKH2024.SbinSolution.Web.Areas.App.Controllers
     [AbpMvcAuthorize(AppPermissions.Pages_RedeemGifts)]
     public class RedeemGiftsController : SbinSolutionControllerBase
     {
-        private readonly IRedeemGiftsAppService  _redeemGiftsAppService;
+        private readonly IRedeemGiftsAppService _redeemGiftsAppService;
+
         public RedeemGiftsController(IRedeemGiftsAppService redeemGiftsAppService)
         {
             _redeemGiftsAppService = redeemGiftsAppService;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
             var input = new GetAllBrandsInput
             {
                 SkipCount = 0,
                 MaxResultCount = 100
             };
-            var brandResult = _redeemGiftsAppService.GetAllBrand(input).Result;
+
+            var brandResult = await _redeemGiftsAppService.GetAllBrand(input);
             ViewBag.Brands = brandResult.Items;
+
             return View();
         }
     }
